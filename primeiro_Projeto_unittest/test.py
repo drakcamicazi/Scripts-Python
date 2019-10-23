9a99f6a55dae28cb01b11eeb9e98e747f503034b
NatielleGoncalves <natiellegoncalvesdesa@usp.br>
2019-10-23 11:30:22 -0300

    Tentando aprender unittest

---
+++ primeiro_Projeto_unittest/test.py
@@ -0,0 +1,23 @@
+import unittest
+from my_sum import sumArq
+
+# Testcase chamado TestSum
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_int_falhara(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 4)
+
+if __name__ == '__main__':
+    unittest.main()
